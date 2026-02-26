a42ee759eee4c6a90e408fad647612ca920d9275
saiveni7 <magantisaiveni@gmail.com>
2026-02-26 09:53:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
